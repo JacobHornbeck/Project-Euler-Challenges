022973907de15dcff0e087e563d4ac8479765a18
Jacob Hornbeck <iamalegomaniac02@gmail.com>
2020-06-12 22:08:27 +0000

    Stuff

--- main.cpp
+++ main.cpp
@@ -9,14 +9,33 @@ void Problem2();
 
 
 int main() {
+    Problem2();
     Problem1();
 }
 
+/* Completed:  */
+void Problem2() {
+    cout << "Begin P2" << endl;
+    int num1 = 1;
+    int num2 = 1;
+    int sum = 0;
+    while (num1<4000000 && num2<4000000) {
+        if (num1%2==0)
+            sum+=num1;
+        else if (num2%2==0)
+            sum+=num2;
+        int temp = num1;
+        num1 = num2;
+        num2 += temp;
+    }
+    cout << "Sum: " << sum << endl;
+    cout << "End P2" << endl << endl;
+}
 
 
-/* Completed Friday, June 12 */
+/* Completed: Friday, June 12 */
 void Problem1() {
-    cout << "Problem1 - Begin" << endl;
+    cout << "Begin P1" << endl;
     int num1 = 3;
     int num2 = 5;
     int num3 = 1000;
@@ -26,5 +45,5 @@ void Problem1() {
             sum += i;
     }
     cout << "Sum: " << sum << endl;
-    cout << "Problem1 - End" << endl << endl;
+    cout << "End P1" << endl << endl;
 }
