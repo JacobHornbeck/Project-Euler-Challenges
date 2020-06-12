ed1c7f2638e4e023f8caaa081ad20346c0d02f11
Jacob Hornbeck <iamalegomaniac02@gmail.com>
2020-06-12 22:32:38 +0000

    Finish Problem2 add Problem3

--- main.cpp
+++ main.cpp
@@ -5,25 +5,38 @@ using namespace std;
 /***Function Prototypes***/
 void Problem1();
 void Problem2();
+void Problem3();
 /*************************/
 
 
 int main() {
+    Problem3();
     Problem2();
     Problem1();
 }
 
 /* Completed:  */
+bool Prime(int n) {
+    for (int i = 0; i < n; i++) {
+        if (n%i==0)
+            return false;
+    }
+    return true;
+}
+void Problem3() {
+
+}
+
+/* Completed: Friday, June 12 */
 void Problem2() {
     cout << "Begin P2" << endl;
     int num1 = 1;
     int num2 = 1;
     int sum = 0;
     while (num1<4000000 && num2<4000000) {
-        if (num1%2==0)
+        if (num1%2==0) {
             sum+=num1;
-        else if (num2%2==0)
-            sum+=num2;
+        }
         int temp = num1;
         num1 = num2;
         num2 += temp;
